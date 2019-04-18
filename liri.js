bcb0730577547e7cf1d38b7ce3fe80442d2dc378
Michael Anderson <mjanders6@gmail.com>
2019-04-17 21:31:47 -0700

    added packages

--- liri.js
+++ liri.js
@@ -1,4 +1,25 @@
 require('dotenv').config()
 const keys = require('./keys.js')
+const [ , , request, search] = process.argv
 
 const spotify = new spotify(keys.spotify)
+
+switch (request) {
+    case 'concert-this':
+
+        break
+    case 'spotify-this-song':
+
+        break
+
+    case 'movie-this':
+
+        break
+
+    case 'do-what-it-says':
+
+        break
+
+    default:
+        break
+}
