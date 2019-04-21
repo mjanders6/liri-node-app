df1ac1bc5322bfe0ce50d1e52ca2abc50fd28572
Michael Anderson <mjanders6@gmail.com>
2019-04-20 18:22:52 -0700

    spotify-this-song created

--- liri.js
+++ liri.js
@@ -1,49 +1,66 @@
 require('dotenv').config()
 var keys = require('./keys.js')
 var Spotify = require('node-spotify-api');
-var [, , request, search] = process.argv
+var [, , request, ...search] = process.argv
 
 var spotify = new Spotify(keys.spotify)
 
- 
-// var spotify = new Spotify({
-//   id: '',
-//   secret: ''
-// });
- 
-spotify
-  .search({ type: 'track', query: 'All the Small Things'})
-  .then(r => {
-    let trackSearch = r.tracks.items
-    //   console.log(r.tracks.items[0].album.artists[0].name);
-    //   console.log(r.tracks.items);
-    trackSearch.forEach((trck, i) => {
-        console.log(r.tracks.items[i].album.artists[0].name);
-        
-    })
+// spotify
+//   .search({ type: 'track', query: search.join(' ')})
+//   .then(r => {
+//     let trackSearch = r.tracks.items
+    
+//     //   console.log(r.tracks.items[0].album.artists[0].name);
+//       // console.log(r.tracks.items[0].name)
+//     trackSearch.forEach((trck, i) => {
+//         console.log(`
+//           Artist: ${r.tracks.items[i].album.artists[0].name}
+//           Song Name: ${r.tracks.items[i].name}
+//           Preview Link: ${r.tracks.items[i].album.external_urls.spotify}
+//           Album: ${r.tracks.items[i].album.name}
+//         `);
+//       })
+//   })    
+//   .catch(function(err) {
+//     console.log(err);
+//   });
 
-  })    
-  .catch(function(err) {
-    console.log(err);
-  });
+switch (request) {
+  case 'concert-this':
 
-// switch (request) {
-//     case 'concert-this':
+    break
 
-//         break
+  case 'spotify-this-song':
+    var spotify = new Spotify(keys.spotify)
 
-//     case 'spotify-this-song':
+    spotify
+      .search({ type: 'track', query: search.join(' ')})
+      .then(r => {
+        let trackSearch = r.tracks.items
+        //   console.log(r.tracks.items[0].album.artists[0].name);
+        // console.log(r.tracks.items[0].name)
+        trackSearch.forEach((trck, i) => {
+          console.log(`
+              Artist: ${r.tracks.items[i].album.artists[0].name}
+              Song Name: ${r.tracks.items[i].name}
+              Preview Link: ${r.tracks.items[i].album.external_urls.spotify}
+              Album: ${r.tracks.items[i].album.name}
+            `);
+        })
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    break
 
-//         break
+  case 'movie-this':
 
-//     case 'movie-this':
+    break
 
-//         break
+  case 'do-what-it-says':
 
-//     case 'do-what-it-says':
+    break
 
-//         break
-
-//     default:
-//         break
-// }
+  default:
+    break
+}
