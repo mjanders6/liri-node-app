696dc7cf526d0621d4c9356acb70d3da87be69ce
Michael Anderson <mjanders6@gmail.com>
2019-04-21 22:35:23 -0700

    update to do-what-it-says

--- liri.js
+++ liri.js
@@ -1,21 +1,24 @@
 require('dotenv').config()
-var keys = require('./keys.js')
-var Spotify = require('node-spotify-api');
+const keys = require('./keys.js')
+const Spotify = require('node-spotify-api');
 const axios = require('axios')
+const fs = require('fs')
 var [, , request, ...search] = process.argv
 
 var spotify = new Spotify(keys.spotify)
 
-let spotSearch = _ => {
+let spotSearch = (goLook) => {
   var spotify = new Spotify(keys.spotify)
 
   let searchTerm = _ => {
-    if (search.join(' ').length < 1) {
-      let search = 'the room'
-      return search
+    if (goLook.join(' ').length < 1) {
+      // let search = 'the room'
+      // console.log(search);
+      return 'the room'
     } else {
-      let search1 = search.join(' ')
-      return search1
+      // let search1 = goLook.join(' ')
+      // console.log(search1);
+      return goLook.join(' ')
     }
   }
 
@@ -39,6 +42,56 @@ let spotSearch = _ => {
     });
 }
 
+let spotMovie = (goLook) => {
+
+  let movieSearch = _ => {
+
+    if (goLook.join(' ').length < 1) {
+      // console.log('Mr. Nobody')
+      return 'Mr. Nobody'
+    } else {
+      // console.log(goLook.join(' '));
+      return goLook.join(' ')
+    }
+  }
+
+  axios.get(`http://www.omdbapi.com/?t=${movieSearch()}&apikey=f8894189`)
+    .then(({ data }) => {
+      // console.log(data)
+      let { Title, Year, imdbRating, Ratings, Country, Language, Plot, Actors } = data
+      console.log(`
+        Title: ${Title}
+        Year: ${Year}
+        imdb Rating: ${imdbRating}
+        Rotten Tomatoes: ${Ratings[1].Value}
+        Country: ${Country}
+        Language: ${Language}
+        Plot: ${Plot}
+        Actors: ${Actors}
+      `)
+    })
+    .catch(e => console.log(e))
+}
+
+const arrFun = (data) => {
+  let arry = []
+  let bestArry = data.split(', ')
+  bestArry.forEach((item, i) => {
+    // console.log(`${item}`)
+    arry.push(item)
+  })
+  // console.log( arry[1].slice(1, arry[1].length - 1) )  
+}
+
+fs.readFile('random.txt', 'UTF8', (e, data) => {
+  if (e) {
+    console.log(e)
+  } else {
+    arrFun(data)
+
+  }
+})
+
 switch (request) {
   case 'concert-this':
     // axios.get(`https://rest.bandsintown.com/artists/${search.join(' ')}/events?app_id=codingbootcamp&date=upcoming`)
@@ -50,67 +103,13 @@ switch (request) {
     break
 
   case 'spotify-this-song':
-  spotSearch()
-    // var spotify = new Spotify(keys.spotify)
+    spotSearch(search)
 
-    // let searchTerm = _ => {
-    //   if (search.join(' ').length < 1) {
-    //     let search = 'the room'
-    //     return search
-    //   } else {
-    //     let search1 = search.join(' ')
-    //     return search1
-    //   }
-    // }
-
-    // spotify
-    //   .search({ type: 'track', query: searchTerm(), limit: 5 })
-    //   .then(r => {
-    //     let trackSearch = r.tracks.items
-
-    //     trackSearch.forEach((trck, i) => {
-
-    //       console.log(`
-    //           Artist: ${trck.album.artists[0].name}
-    //           Song Name: ${trck.name}
-    //           Preview Link: ${trck.album.external_urls.spotify}
-    //           Album: ${trck.album.name}
-    //         `);
-    //     })
-    //   })
-    //   .catch(function (err) {
-    //     console.log(err);
-    //   });
     break
 
   case 'movie-this':
+    spotMovie(search)
 
-    let movieSearch = _ => {
-      if (search.join(' ').length < 1) {
-        let search = 'Mr. Nobody'
-        return search
-      } else {
-        let search1 = search.join(' ')
-        return search1
-      }
-    }
-
-    axios.get(`http://www.omdbapi.com/?t=${movieSearch()}&apikey=f8894189`)
-      .then(({ data }) => {
-        // console.log(data)
-        let { Title, Year, imdbRating, Ratings, Country, Language, Plot, Actors } = data
-        console.log(`
-          Title: ${Title}
-          Year: ${Year}
-          imdb Rating: ${imdbRating}
-          Rotten Tomatoes: ${Ratings[1].Value}
-          Country: ${Country}
-          Language: ${Language}
-          Plot: ${Plot}
-          Actors: ${Actors}
-        `)
-      })
-      .catch(e => console.log(e))
     break
 
   case 'do-what-it-says':
