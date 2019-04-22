966683417f41e2f2e7ec2dad9391e3a6c87e151b
Michael Anderson <mjanders6@gmail.com>
2019-04-21 20:52:43 -0700

    movie search into a function

--- liri.js
+++ liri.js
@@ -6,6 +6,39 @@ var [, , request, ...search] = process.argv
 
 var spotify = new Spotify(keys.spotify)
 
+let spotSearch = _ => {
+  var spotify = new Spotify(keys.spotify)
+
+  let searchTerm = _ => {
+    if (search.join(' ').length < 1) {
+      let search = 'the room'
+      return search
+    } else {
+      let search1 = search.join(' ')
+      return search1
+    }
+  }
+
+  spotify
+    .search({ type: 'track', query: searchTerm(), limit: 5 })
+    .then(r => {
+      let trackSearch = r.tracks.items
+
+      trackSearch.forEach((trck, i) => {
+
+        console.log(`
+            Artist: ${trck.album.artists[0].name}
+            Song Name: ${trck.name}
+            Preview Link: ${trck.album.external_urls.spotify}
+            Album: ${trck.album.name}
+          `);
+      })
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
 switch (request) {
   case 'concert-this':
     // axios.get(`https://rest.bandsintown.com/artists/${search.join(' ')}/events?app_id=codingbootcamp&date=upcoming`)
@@ -17,36 +50,37 @@ switch (request) {
     break
 
   case 'spotify-this-song':
-    var spotify = new Spotify(keys.spotify)
+  spotSearch()
+    // var spotify = new Spotify(keys.spotify)
 
-    let searchTerm = _ => {
-      if (search.join(' ').length < 1) {
-        let search = 'the room'
-        return search
-      } else {
-        let search1 = search.join(' ')
-        return search1
-      }
-    }
+    // let searchTerm = _ => {
+    //   if (search.join(' ').length < 1) {
+    //     let search = 'the room'
+    //     return search
+    //   } else {
+    //     let search1 = search.join(' ')
+    //     return search1
+    //   }
+    // }
 
-    spotify
-      .search({ type: 'track', query: searchTerm(), limit: 5 })
-      .then(r => {
-        let trackSearch = r.tracks.items
+    // spotify
+    //   .search({ type: 'track', query: searchTerm(), limit: 5 })
+    //   .then(r => {
+    //     let trackSearch = r.tracks.items
 
-        trackSearch.forEach((trck, i) => {
+    //     trackSearch.forEach((trck, i) => {
 
-          console.log(`
-              Artist: ${trck.album.artists[0].name}
-              Song Name: ${trck.name}
-              Preview Link: ${trck.album.external_urls.spotify}
-              Album: ${trck.album.name}
-            `);
-        })
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
+    //       console.log(`
+    //           Artist: ${trck.album.artists[0].name}
+    //           Song Name: ${trck.name}
+    //           Preview Link: ${trck.album.external_urls.spotify}
+    //           Album: ${trck.album.name}
+    //         `);
+    //     })
+    //   })
+    //   .catch(function (err) {
+    //     console.log(err);
+    //   });
     break
 
   case 'movie-this':
